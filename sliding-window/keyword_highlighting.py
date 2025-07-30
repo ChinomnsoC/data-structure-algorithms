0244721b961842e9a934e75cda5420dff8837705
Chinomnso Chinedum <chinomnsoc@gmail.com>
2025-07-30 14:55:16 +0100

    Updating my first keyword highlighting version

---
+++ sliding-window/keyword_highlighting.py
@@ -0,0 +1,61 @@
+# // Example 1:
+# Input: keyword = "fig", text = "figma"
+# Output: "<b>fig</b>ma"
+
+# // Example 2:
+# Input: keyword = "ana", text = "banana"
+# Output: "b<b>anana</b>"  
+# // NOT "b<b>ana</b>na" - we want to bold the maximum coverage
+
+
+def find_matches(keyword: str, text: str):
+    matches = []
+    
+    for i in range(len(text) - len(keyword)+1):
+        print("checking")
+        if text[i:i + len(keyword)] == keyword:
+            matches.append((i, i + len(keyword)-1))
+            print(matches)
+    
+    if not matches:
+        return text
+        
+    merged_intervals = [matches[0]]
+        
+    for match in matches[1:]:
+        if match[0] <= merged_intervals[-1][1]:
+            merged = [min(match[0], merged_intervals[-1][0]), max(match[1], merged_intervals[-1][1])]
+            merged_intervals[-1] = merged
+        else:
+            merged_intervals.append(match)
+            
+    # use the values in the tuples in matches to add the B tags.
+    # // Build result string with bold tags
+    result = ""
+    lastIndex = 0
+    
+    for start, end in merged_intervals:
+        result += text[lastIndex:start]
+        print("result 1 and lastIndex 1", result, lastIndex)
+        result += f"<b>{text[start:end + 1]}</b>"
+        print("result 2 and lastIndex 2", result, lastIndex)
+        lastIndex = end + 1
+        print("result 3 and lastIndex 3", result, lastIndex)
+        
+        
+    print("merged intervals", merged_intervals)
+    return result
+                    
+                    
+print(find_matches("ana", "bananaavocadoana"))
+
+    
+# def merge_intervals(self, interval, last_interval_in_matches):
+#     merged_intervals = [min(interval + last_interval_in_matches), max(interval + last_interval_in_matches)]
+#     return merged_intervals
+    # # [[1, 3], [3, 5]]
+
+    # for match in matches[1:]:
+    #     if match[0] <= matches[-1][1]:
+    #         # merge intervals
+    #     return
