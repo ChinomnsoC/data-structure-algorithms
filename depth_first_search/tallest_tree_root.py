ab4ee7d31b526144b63775f9b0b4fad1c3fd81cf
Chinomnso Chinedum <chinomnsoc@gmail.com>
2026-02-27 10:36:28 +0000

    solved a variation of tallest tree

--- depth_first_search/tallest_tree_root.py
+++ depth_first_search/tallest_tree_root.py
@@ -1,3 +1,53 @@
+def tallest_tree_root_variation(forest: dict) -> int:
+    if not forest:
+        return 0
+    
+    children = set(forest.keys())
+    all_nodes = set(forest.keys()) | set(forest.values())
+    
+    roots = all_nodes - children
+    
+    # build the adjacency list
+    adj_list = {}
+    
+    for child, parent in forest.items():
+        if parent not in adj_list:
+            adj_list[parent] = []
+        
+        adj_list[parent].append(child)
+    
+    # write the dfs recursion
+    def dfs_height(node, adj_list):
+        # check if node in adj_list -- do something
+        if node not in adj_list:
+            return 1
+        # check if child is in adj list
+        # can replace with this
+        # return 1 + max(dfs_height(child, adj_list) for child in adj_list[node])
+        max_height = 0
+        for child in adj_list[node]:
+            max_height = max(max_height, dfs_height(child, adj_list))
+        return 1 + max_height
+    
+    root_map = {}
+    for root in roots:
+        height = dfs_height(root, adj_list)
+        root_map[root] = height
+    
+    max_root_height = max(root_map.values())
+    
+    ties = []
+    for root, height in root_map.items():
+        if height == max_root_height:
+            ties.append(root)
+    
+    return min(ties)
+        
+        
+
+    # call dfs on all each root in roots
+    # return the min node with max height
+ 
 def tallest_tree_root(forest: dict) -> int:
     if not forest:
         return 0
