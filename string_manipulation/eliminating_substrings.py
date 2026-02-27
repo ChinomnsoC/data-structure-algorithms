e889f09ff84506da22ec0490ec98d6a485167429
Chinomnso Chinedum <chinomnsoc@gmail.com>
2026-02-27 11:13:54 +0000

    solved eliminate substrings

---
+++ string_manipulation/eliminating_substrings.py
@@ -0,0 +1,46 @@
+def eliminate_substring(s: str) -> str:
+    target = "AWS"
+    
+    stack = []
+    
+    for char in s:
+        stack.append(char)
+        
+        if stack[-3:] == ['A', 'W', 'S']:
+            stack.pop(); stack.pop(); stack.pop()
+    
+    result = "".join(stack)
+    
+    return result if result else "-1"
+
+def eliminate_substring_variation(s: str, target: str) -> str:
+    
+    stack = []
+    tgt_length = len(target)
+    
+    for char in s:
+        stack.append(char)
+        
+        if stack[-tgt_length:] == list(target):
+            # call stack.pop tgt_length number of times
+            for _ in range(tgt_length):
+                stack.pop()
+            pass
+    result = "".join(stack)
+    
+    return result if result else "-1"
+
+def do_tests_pass() -> bool:
+    assert eliminate_substring("AWAWSSG") == "G"
+    assert eliminate_substring("AWAWSS") == "-1"
+    assert eliminate_substring("AWS") == "-1"
+    assert eliminate_substring("AWSAWS") == "-1"
+    assert eliminate_substring("HELLO") == "HELLO"
+    assert eliminate_substring("") == "-1"
+    return True
+
+if __name__ == "__main__":
+    if do_tests_pass():
+        print("All tests pass")
+    else:
+        print("Tests fail")
