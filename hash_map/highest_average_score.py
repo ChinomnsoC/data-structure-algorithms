60fe3e745fbd457bf41a9fdc48d62effdcd3fbab
Chinomnso Chinedum <chinomnsoc@gmail.com>
2026-03-08 21:59:18 +0000

    solved highest average score with better optimisation, one for loop

--- hash_map/highest_average_score.py
+++ hash_map/highest_average_score.py
@@ -1,45 +1,54 @@
 def highest_average_score(scores: list) -> int:
     scores_map = {}
     
+    if not scores:
+        return 0
+    
     for student_result in scores:
-        if student_result[0] not in scores_map:
-            scores_map[student_result[0]] = []
-        scores_map[student_result[0]].append(student_result[1])
-    
-    # find the average of each key and update the values in the map
-    for student_id, scores_list in scores_map.items():
-        average_score = sum(scores_list) // len(scores_list)
+        if not student_result:
+            return 0
+        student_name = student_result[0]
+        if student_name not in scores_map:
+            scores_map[student_name] = [0, 0]
         
-        scores_map[student_id] = average_score
-    
-    highest_score = max(scores_map.values())
-    ties = []
-    
-    for student_id, average_score in scores_map.items():
-        if average_score == highest_score:
-            ties.append(student_id)
-    
-    winner = min(ties)
-    
-    return winner
+        score = int(float(student_result[1]))
+
+        # scores_map = {"name": [total_score, count]}
+        scores_map[student_name][0] += score
+        scores_map[student_name][1] += 1
+        
+    return max(total_score // count for total_score, count in scores_map.values())
         
 
 def do_tests_pass() -> bool:
     # Basic case
-    scores1 = [[1, 91], [1, 92], [2, 93], [2, 97], [1, 60], [2, 77], [1, 61]]
-    assert highest_average_score(scores1) == 2  # avg: 1->76, 2->89
+    scores1 = [["Ben", "91"], ["Murray", "92"], ["Bruce", "93"], ["Angela", "97"], ["Ben", "60"], ["Murray", "77"], ["Bruce", "61"]]
+    
+    print(highest_average_score(scores1))
+    assert highest_average_score(scores1) == 97  # avg: 1->76, 2->89
 
     # Tie - return smaller ID
-    scores2 = [[1, 100], [2, 100]]
-    assert highest_average_score(scores2) == 1
+    scores2 = [[]]
+    assert highest_average_score(scores2) == 0
 
-    # Single student
-    scores3 = [[1, 85], [1, 90]]
-    assert highest_average_score(scores3) == 1
+    # # Single student
+    scores3 = [["Ben", "-91"], ["Murray", "-92"], ["Bruce", "93"], ["Angela", "-7"], ["Ben", "60"], ["Murray", "77"], ["Bruce", "61"]]
+    print(highest_average_score(scores3))
+    assert highest_average_score(scores3) == 77
 
-    # From your original list
-    scores4 = [[1, 87], [2, 35], [1, 52], [3, 35], [2, 55], [4, 99]]
-    assert highest_average_score(scores4) == 4  # Jessica=99, avg: 1->69, 2->45, 3->35, 4->99
+    # # From your original list
+    scores4 = [["Ben", "-91"], ["Murray", "-92"], ["Bruce", "-93"], ["Angela", "-7.5"], ["Ben", "-60"], ["Murray", "-77"], ["Bruce", "-61"]]
+    print(highest_average_score(scores4))
+    assert highest_average_score(scores4) == -7
+    
+    scores5 = []
+    assert highest_average_score(scores5) == 0
+    
+    scores6 = [["Ben", "-91"]]
+    assert highest_average_score(scores6) == -91
+    
+    scores7 = [["Ben", "0"], ["Murray", "0"], ["Bruce", "0"]]
+    assert highest_average_score(scores7) == 0
 
     return True
 
@@ -48,3 +57,6 @@ if __name__ == "__main__":
         print("All tests pass")
     else:
         print("Tests fail")
+        
+        
+#  scores1 = [["Bob", "87"], ["Mike", "35"], ["Bob", "52"]]
