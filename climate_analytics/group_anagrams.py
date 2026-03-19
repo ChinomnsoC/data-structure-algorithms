a15585d6c13939eeca68dca35954fe419e35c54d
Chinomnso Chinedum <chinomnsoc@gmail.com>
2026-03-19 17:45:57 +0000

    solved group anagrams with two bugs

---
+++ climate_analytics/group_anagrams.py
@@ -0,0 +1,21 @@
+
+def groupAnagrams(strs):
+    
+    anagrams_map = {}
+    
+    for word in strs:
+        count_key = [0] * 26
+        
+        for char in word:
+            index = ord(char) - ord('a')
+            count_key[index] += 1
+            
+        key = tuple(count_key)
+        
+        if key not in anagrams_map:
+            anagrams_map[key] = []
+        
+        anagrams_map[key].append(word)
+    
+    return list(anagrams_map.values())
+            
