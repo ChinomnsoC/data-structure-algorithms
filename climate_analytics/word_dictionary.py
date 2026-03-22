58e70e5b312af408c2736772fbe1fed53994d566
Chinomnso Chinedum <chinomnsoc@gmail.com>
2026-03-22 19:16:49 +0000

    solve word dictionary

---
+++ climate_analytics/word_dictionary.py
@@ -0,0 +1,64 @@
+# pythontrainingData = [
+#     ["I", "am", "Sam"],
+#     ["Sam", "I", "am"],
+#     ["Green", "Eggs", "I", "like"],
+#     ["Green", "Eggs", "and", "ham"]
+# ]
+# ```
+
+# Implement a class `AutoComplete` with:
+
+# **Part 1:**
+# - `__init__(self, trainingData, badWords)` — initialise the model
+# - `getBestWord(self, inputString)` — return the most frequent next word for a given input word, or empty string if none exists
+# ```
+# getBestWord("I") → "am"  # "am" follows "I" twice, "like" once
+# getBestWord("Green") → "Eggs"
+# getBestWord("xyz") → ""
+
+from collections import defaultdict, Counter
+
+training_data = [
+    ["I", "am", "Sam"],
+    ["Sam", "I", "am"],
+    ["Green", "Eggs", "I", "like"],
+    ["Green", "Eggs", "and", "ham"]
+]
+class Autocomplete():
+    def __init__(self, trainingData, badWords):
+        self.trainingData = trainingData
+        self.badWords = badWords
+        
+        self.data_map = defaultdict(Counter)
+        
+        self._build_map(self.trainingData)
+        
+
+
+    def getBestWord(self, inputString):
+        inputString = inputString.lower()
+        if inputString not in self.data_map:
+            return ""
+        
+        freq_dict = self.data_map[inputString]
+        
+        return max(freq_dict, key=freq_dict.get)
+    
+    def updateTrainingData(self, newTrainingData):
+        
+        self._build_map(newTrainingData)
+    
+    def _build_map(self, trainingData):
+        
+        for sentence in trainingData:
+            for i in range(len(sentence) - 1):
+                current_word = sentence[i].lower()
+                next_word = sentence[i+ 1].lower()
+                
+                if current_word not in self.badWords and next_word not in self.badWords:
+                    self.data_map[current_word][next_word] += 1
+
+autocomplete = Autocomplete(trainingData=training_data, badWords=["ugly"])
+
+print(autocomplete.getBestWord("I"))
+print(autocomplete.getBestWord("Am")) 
