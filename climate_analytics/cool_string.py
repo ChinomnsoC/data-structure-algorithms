1a0e91c56bb1c5c71fbd63daf08f60ba4ead5c97
Chinomnso Chinedum <chinomnsoc@gmail.com>
2026-03-30 09:24:58 +0100

    removed bug from cool string

--- climate_analytics/cool_string.py
+++ climate_analytics/cool_string.py
@@ -17,27 +17,34 @@ def cool_string(s):
         s_freq_map[char] = s_freq_map.get(char, 0) + 1
         
     print(s_freq_map)
+    counts_freq_values = list(s_freq_map.values())
     max_freq = max(s_freq_map.values())
-    diff_freq =  max_freq - min(s_freq_map.values())
+    min_freq = min(s_freq_map.values())
+    diff_freq =  max_freq - min_freq
     
-    if diff_freq <= 1:
-        if diff_freq == 0:
-            return True
-        count_max_char = 0
-        for char, count in s_freq_map.items():
-            if count == max_freq:
-                count_max_char += 1
-                
-        if count_max_char == 1:
-            return True
-        else:
-            return False
+    if diff_freq == 0:
+        return True
     
-    else: 
-        return False
-        
+    if diff_freq == 1:
+        # only one char has the max 
+        if counts_freq_values.count(max_freq) == 1:
+            return True
+            
+        # only one char is at min
+        if counts_freq_values.count(min_freq) == 1:
+            return True
+    
+    if max_freq == 1:
+        return True
+    
+    # one char appears once, but the rest are equal.
+    if counts_freq_values.count(1) == 1 and len(set(counts_freq_values) - {1} ) == 1:
+        return True
+    
+    return False
 
 print(cool_string("aaabbb")) # → True   (a:3, b:3 — already equal)
 print(cool_string("aaabbbcccc")) # → True   (remove one c → a:3, b:3, c:3)
 print(cool_string("aaabbbccccdddd")) # → False  (need to remove 2)
 print(cool_string("aabbcccdddeeee")) # → False
+print(cool_string("abbbcccdddeee"))
